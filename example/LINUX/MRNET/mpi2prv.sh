20c3a2ee656c2a23de46b8b9d716fe86b9a61b03
Germán Llort <gllort@bsc.es>
2010-02-16 12:10:32 +0000

    Updated MRNet example

--- example/LINUX/MRNET/mpi2prv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export MPITRACE_HOME=@sed_MYPREFIXDIR@
-
-TRACE_NAME=trace_mpi_ping.prv
-
-${MPITRACE_HOME}/bin/mpi2prv -f TRACE.mpits -o ${TRACE_NAME}
-
---
+++ example/LINUX/MRNET/run.cmd
@@ -0,0 +1,22 @@
+#!/bin/bash
+# @ class = debug
+# @ initialdir = .
+# @ output = trace%j.out
+# @ error =  trace%j.err
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!! REQUEST AT LEAST 1 EXTRA TASK !!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# @ total_tasks = 33
+# @ cpus_per_task = 1
+# @ tasks_per_node = 4
+# @ wall_clock_limit = 00:30:00
+
+export MPITRACE_HOME=/home/bsc41/bsc41127/Tools/mpitrace_mrnet2.0/mpitrace_svn20100211/Package/64
+
+### Set how many tasks will the application run with. 
+### Note that you have to allocate extra resources above (at least 1 more task).
+APPL_NAME=./test
+APPL_NPROCS=32
+
+### Run the application with: mrnrun <nprocs> ./trace.sh <appl>
+time ${MPITRACE_HOME}/bin/mrnrun ${APPL_NPROCS} ./trace.sh ${APPL_NAME}
