6b2b3a510b811f1c32192219497df102de7b3ba1
Harald Servat <harald.servat@bsc.es>
2010-04-12 13:13:52 +0000

    Added macro MARK_SET_READ when no performance counters are used in the instrumentation package.

--- src/tracer/trace_hwc.h
+++ src/tracer/trace_hwc.h
@@ -92,6 +92,7 @@
 # define COPY_ACCUMULATED_COUNTERS_HERE(tid, evt)
 # define ADD_ACCUMULATED_COUNTERS_HERE(tid, evt)
 # define HARDWARE_COUNTERS_CHANGE(time, tid)
+# define MARK_SET_READ(tid, evt, filter)
 
 #endif
 
