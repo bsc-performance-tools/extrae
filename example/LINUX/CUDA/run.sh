1e30824426ae754ef983d49fd76366b786ae28b6
Harald Servat <harald.servat@bsc.es>
2011-08-23 10:17:34 +0000

    Minor adjustaments to the CUDA examples

--- example/LINUX/CUDA/run.sh
+++ example/LINUX/CUDA/run.sh
@@ -4,3 +4,4 @@ export EXTRAE_HOME=@sub_PREFIXDIR@
 export PAPI_HOME=@sub_PAPI_HOME@
 
 EXTRAE_CONFIG_FILE=extrae.xml LD_LIBRARY_PATH=${EXTRAE_HOME}/lib:${PAPI_HOME}/lib:${LD_LIBRARY_PATH} ./hello
+${EXTRAE_HOME}/bin/mpi2prv -f TRACE.mpits -e ./hello
