7bfd11c79a38fdc8317539c327dde5c773a91334
Harald Servat <harald.servat@bsc.es>
2011-08-23 09:58:23 +0000

    Added example for CUDA instrumentation

---
+++ example/LINUX/CUDA/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export EXTRAE_HOME=@sub_PREFIXDIR@
+export PAPI_HOME=@sub_PAPI_HOME@
+
+EXTRAE_CONFIG_FILE=extrae.xml LD_LIBRARY_PATH=${EXTRAE_HOME}/lib:${PAPI_HOME}/lib:${LD_LIBRARY_PATH} ./hello
