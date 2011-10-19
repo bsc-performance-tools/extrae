5771503acda36d483eaf34b31f7e6ba095de858e
Harald Servat <harald.servat@bsc.es>
2011-10-19 09:26:33 +0000

    Updated examples to use appropiate SLURM_PROCID

--- example/LINUX/MPI/dyninst/run.sh
+++ example/LINUX/MPI/dyninst/run.sh
@@ -4,8 +4,8 @@ export EXTRAE_HOME=@sub_PREFIXDIR@
 source ${EXTRAE_HOME}/etc/extrae.sh
 
 # Only show output for task 0, others task send output to /dev/null
-if test "${MXMPI_ID}" == "0" ; then
-	${EXTRAE_HOME}/bin/ompitrace -config ../extrae.xml $@ > job${SLURM_JOB_ID}.${MXMPI_ID}.out 2> job${SLURM_JOB_ID}.${MXMPI_ID}.err
+if test "${SLURM_PROCID}" == "0" ; then
+	${EXTRAE_HOME}/bin/ompitrace -config ../extrae.xml $@ > job${SLURM_JOB_ID}.${SLURM_PROCID}.out 2> job${SLURM_JOB_ID}.${SLURM_PROCID}.err
 else
 	${EXTRAE_HOME}/bin/ompitrace -config ../extrae.xml $@ > /dev/null 2> /dev/null
 fi
