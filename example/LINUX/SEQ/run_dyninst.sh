d93e10bf14cd40a591f323949be981ca99be95a0
Harald Servat <harald.servat@bsc.es>
2011-10-25 10:23:01 +0000

    Renaming dyninst-based launcher: ompitrace -> extrae

--- example/LINUX/SEQ/run_dyninst.sh
+++ example/LINUX/SEQ/run_dyninst.sh
@@ -4,6 +4,6 @@ export EXTRAE_HOME=@sub_PREFIXDIR@
 export LD_LIBRARY_PATH=${EXTRAE_HOME}/lib
 source ${EXTRAE_HOME}/etc/extrae.sh
 
-${EXTRAE_HOME}/bin/ompitrace -config extrae.xml ./pi
+${EXTRAE_HOME}/bin/extrae -config extrae.xml ./pi
 
 ${EXTRAE_HOME}/bin/mpi2prv -e pi -f TRACE.mpits -s TRACE.sym -o pi.prv
