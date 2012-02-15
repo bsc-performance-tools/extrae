39f34e0748993759d5ebe9de086a0c105baf684a
Harald Servat <harald.servat@bsc.es>
2012-02-15 10:26:54 +0000

    LD_PRELOAD is not needed in these cases

--- example/FREEBSD/OMP/run_ldpreload.sh
+++ example/FREEBSD/OMP/run_ldpreload.sh
@@ -3,5 +3,6 @@
 export OMP_NUM_THREADS=4
 export EXTRAE_CONFIG_FILE=extrae.xml
 export EXTRAE_HOME=@sub_PREFIXDIR@
+source ${EXTRAE_HOME}/etc/extrae.sh
 
-LD_PRELOAD=${EXTRAE_HOME}/lib/libomptrace.so ./pi_instrumented
+./pi_instrumented
--- example/LINUX/OMP/run_ldpreload.sh
+++ example/LINUX/OMP/run_ldpreload.sh
@@ -5,4 +5,4 @@ export EXTRAE_CONFIG_FILE=extrae.xml
 export EXTRAE_HOME=@sub_PREFIXDIR@
 source ${EXTRAE_HOME}/etc/extrae.sh
 
-LD_PRELOAD=${EXTRAE_HOME}/lib/libomptrace.so ./pi_instrumented
+./pi_instrumented
