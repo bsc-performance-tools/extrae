cc8d5ea3955699411844ff9f7ffb5b0bd6778348
Harald Servat <harald.servat@bsc.es>
2012-03-06 15:52:53 +0000

    Added user-guide to the SVN

---
+++ doc/user-guide/images.pl
@@ -0,0 +1,37 @@
+# LaTeX2HTML 2008 (1.71)
+# Associate images original text with physical files.
+
+
+$key = q/{landscape}%latex2htmlidmarker3450{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="990" HEIGHT="612" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{landscape}
+% latex2html id marker 3450\par
+\begin{table}
+\centerline{
+\...
+... to configure {\sf {E}xtrae} ({\em continued})}
+\end{table}\par
+\end{landscape}">|; 
+
+$key = q/dollarast;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$\$\ast$">|; 
+
+$key = q/{landscape}%latex2htmlidmarker3429table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="990" HEIGHT="696" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="\begin{landscape}
+% latex2html id marker 3429\begin{table}
+\centerline{
+\begin...
+...riables available to configure {\sf {E}xtrae} }
+\end{table}\par
+\end{landscape}">|; 
+
+1;
+
