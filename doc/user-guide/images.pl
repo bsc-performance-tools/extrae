524b7ba01ab7d3b226033bc420a1a38a671e7802
Harald Servat <harald.servat@bsc.es>
2012-10-04 09:58:57 +0000

    Added section Check in Chapter 3

--- doc/user-guide/images.pl
+++ doc/user-guide/images.pl
@@ -9,30 +9,30 @@ $cached_env_img{$key} = q|<IMG
  ALT="\begin{figure}\centering
 \psfig{figure=images/extrae-post-installation.eps, width=12cm}\end{figure}">|; 
 
-$key = q/{landscape}%latex2htmlidmarker3661table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="989" HEIGHT="697" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="\begin{landscape}
-% latex2html id marker 3661\begin{table}
-\centerline{
-\begin...
-...riables available to configure {\sf {E}xtrae} }
-\end{table}\par
-\end{landscape}">|; 
-
 $key = q/dollarast;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="22" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
  ALT="$\$\ast$">|; 
 
-$key = q/{landscape}%latex2htmlidmarker3682{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$key = q/{landscape}%latex2htmlidmarker3677table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="989" HEIGHT="697" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="\begin{landscape}
+% latex2html id marker 3677\begin{table}
+\centerline{
+\begin...
+...riables available to configure {\sf {E}xtrae} }
+\end{table}\par
+\end{landscape}">|; 
+
+$key = q/{landscape}%latex2htmlidmarker3698{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="989" HEIGHT="613" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img4.png"
  ALT="\begin{landscape}
-% latex2html id marker 3682\par
+% latex2html id marker 3698\par
 \begin{table}
 \centerline{
 \...
