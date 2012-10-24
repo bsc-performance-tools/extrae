d3a04d9eb527b51b73220bdf4bc7b61aeff7deb2
Harald Servat <harald.servat@bsc.es>
2012-10-24 14:02:20 +0000

    Fix information relative to make check

--- doc/user-guide/images.pl
+++ doc/user-guide/images.pl
@@ -2,6 +2,19 @@
 # Associate images original text with physical files.
 
 
+$key = q/{landscape}%latex2htmlidmarker3789{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="989" HEIGHT="613" ALIGN="BOTTOM" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="\begin{landscape}
+% latex2html id marker 3789\par
+\begin{table}
+\centerline{
+\...
+... to configure {\sf {E}xtrae} ({\em continued})}
+\end{table}\par
+\end{landscape}">|; 
+
 $key = q/{figure}{centering{{psfig{figure=imagesslashextrae-post-installation.eps,width=12cm}{{{{figure};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="542" HEIGHT="364" BORDER="0"
@@ -15,28 +28,15 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img2.png"
  ALT="$\$\ast$">|; 
 
-$key = q/{landscape}%latex2htmlidmarker3756table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$key = q/{landscape}%latex2htmlidmarker3768table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="989" HEIGHT="697" ALIGN="BOTTOM" BORDER="0"
  SRC="|."$dir".q|img3.png"
  ALT="\begin{landscape}
-% latex2html id marker 3756\begin{table}
+% latex2html id marker 3768\begin{table}
 \centerline{
 \begin...
 ...riables available to configure {\sf {E}xtrae} }
-\end{table}\par
-\end{landscape}">|; 
-
-$key = q/{landscape}%latex2htmlidmarker3777{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="989" HEIGHT="613" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="\begin{landscape}
-% latex2html id marker 3777\par
-\begin{table}
-\centerline{
-\...
-... to configure {\sf {E}xtrae} ({\em continued})}
 \end{table}\par
 \end{landscape}">|; 
 
