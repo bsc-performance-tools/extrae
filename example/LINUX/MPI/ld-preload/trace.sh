dfdbf3051e000f8b653ca02bb0e82b417149ee37
Harald Servat <harald.servat@bsc.es>
2013-12-19 13:43:55 +0000

    Fix usage of libmpitracef

--- example/LINUX/MPI/ld-preload/trace.sh
+++ example/LINUX/MPI/ld-preload/trace.sh
@@ -2,8 +2,8 @@
 
 export EXTRAE_HOME=@sub_PREFIXDIR@
 export EXTRAE_CONFIG_FILE=../extrae.xml
-export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so
-#export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitracef.so
+#export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so # For C apps
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitracef.so # For Fortran apps
 
 ## Run the desired program
 $*
