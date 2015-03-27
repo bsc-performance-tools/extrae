ec07f19bf9fbed5229cb9033c67feb4c69dde00a
Harald Servat <harald.servat@bsc.es>
2015-03-27 16:46:18 +0000

    Update documentation

--- doc/user-guide/images.pl
+++ doc/user-guide/images.pl
@@ -9,74 +9,36 @@ $cached_env_img{$key} = q|<IMG
  ALT="\begin{figure}\centering
 \psfig{figure=images/extrae-post-installation.eps, width=12cm}\end{figure}">|; 
 
-$key = q/{lstlisting}[language=XML,frame=single]<gremlinsstart="0"increment="2"roundtrip="no"loop="no"slash>{lstlisting};LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="633" HEIGHT="34" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="\begin{lstlisting}[language=XML,frame=single]
-&lt;gremlins start=''0'' increment=''2'' roundtrip=''no'' loop=''no''/&gt;
-\end{lstlisting}">|; 
-
-$key = q/{lstlisting}[language=XML,frame=single]<onlineenabled="yes"analysis="clustering"frequency="auto"topology="auto">{lstlisting};LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="99" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="\begin{lstlisting}[language=XML,frame=single]
-&lt;online enabled=''yes''
-analysis=''clustering''
-frequency=''auto''
-topology=''auto''&gt;
-\end{lstlisting}">|; 
-
 $key = q/dollarast;MSF=1.6;LFS=11;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="22" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img6.png"
+ SRC="|."$dir".q|img2.png"
  ALT="$\$\ast$">|; 
 
-$key = q/{lstlisting}[language=XML,frame=single]<clusteringconfig="cl.I.IPC.xml"slash>{lstlisting};LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="567" HEIGHT="34" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="\begin{lstlisting}[language=XML,frame=single]
-&lt;clustering config=''cl.I.IPC.xml''/&gt;
-\end{lstlisting}">|; 
-
-$key = q/{landscape}%latex2htmlidmarker4318table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$key = q/{landscape}%latex2htmlidmarker4317table{{centerline{tabular{{|p{7cm}|p{14cm}|}hlablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="990" HEIGHT="697" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img7.png"
+ SRC="|."$dir".q|img3.png"
  ALT="\begin{landscape}
-% latex2html id marker 4318\begin{table}
+% latex2html id marker 4317\begin{table}
 \centerline{
 \begin...
 ...riables available to configure {\sf {E}xtrae} }
 \end{table}\par
 \end{landscape}">|; 
 
-$key = q/{landscape}%latex2htmlidmarker4339{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
+$key = q/{landscape}%latex2htmlidmarker4338{par{table{{centerline{tabular{{|p{7cm}|p{14cmoconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="990" HEIGHT="635" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img8.png"
+ SRC="|."$dir".q|img4.png"
  ALT="\begin{landscape}
-% latex2html id marker 4339\par
+% latex2html id marker 4338\par
 \begin{table}
 \centerline{
 \...
 ... to configure {\sf {E}xtrae} ({\em continued})}
 \end{table}\par
 \end{landscape}">|; 
-
-$key = q/{lstlisting}[language=XML,frame=single]<spectralmax_periods="0"num_iters="3"min_uration="3s"slash><slashspectral_advanced><slashspectral>{lstlisting};LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="748" HEIGHT="142" ALIGN="BOTTOM" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="\begin{lstlisting}[language=XML,frame=single]
-&lt;spectral max_periods=''0'' num_it...
-...rsts'' min_duration=''3s''/&gt;
-&lt;/spectral_advanced&gt;
-&lt;/spectral&gt;
-\end{lstlisting}">|; 
 
 1;
 
