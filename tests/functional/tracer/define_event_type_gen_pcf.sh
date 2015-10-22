e2ed4b2c1dcf956d08e8581dd11496da0c3032f4
Harald Servat <harald.servat@bsc.es>
2015-10-22 10:22:35 +0000

    Add --without-address to avoid comparing if the package has been configured to provide information for memory addresses

--- tests/functional/tracer/define_event_type_gen_pcf.sh
+++ tests/functional/tracer/define_event_type_gen_pcf.sh
@@ -3,7 +3,7 @@
 rm -fr *.sym *.mpits set-0
 
 EXTRAE_ON=1 ./define_event_type_gen_pcf
-../../../src/merger/mpi2prv -f TRACE.mpits -e .libs/define_event_type_gen_pcf -o define_event_type_gen_pcf.prv
+../../../src/merger/mpi2prv -without-addresses -f TRACE.mpits -e .libs/define_event_type_gen_pcf -o define_event_type_gen_pcf.prv
 
 # Actual comparison
 diff define_event_type_gen_pcf.reference define_event_type_gen_pcf.pcf
