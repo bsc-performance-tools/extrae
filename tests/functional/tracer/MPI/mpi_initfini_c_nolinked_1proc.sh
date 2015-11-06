a04f1f30662157ee3baea9b1271446687fd9e611
Harald Servat <harald.servat@bsc.es>
2015-11-06 15:47:58 +0000

    Some more tests

--- tests/functional/tracer/MPI/mpi_initfini_c_nolinked_1proc.sh
+++ tests/functional/tracer/MPI/mpi_initfini_c_nolinked_1proc.sh
@@ -19,6 +19,18 @@ NENTRIES_INIT=`grep :50000003:31 ${TRACE}.prv | wc -l`
 NENTRIES_FINI=`grep :50000003:32 ${TRACE}.prv | wc -l`
 NEXITS=`grep :50000003:0 ${TRACE}.prv | wc -l`
 
+NENTRIES_INIT_PCF=`grep MPI_Init ${TRACE}.pcf | wc -l`
+NENTRIES_FINI_PCF=`grep MPI_Finalize ${TRACE}.pcf | wc -l`
+
+if [[ "${NENTRIES_INIT_PCF}" -ne 1 ]] ; then
+	echo "There must be only one entry to MPI_Init in the PCF"
+	exit 1
+fi
+if [[ "${NENTRIES_FINI_PCF}" -ne 1 ]] ; then
+	echo "There must be only one entry to MPI_Finalize in the PCF"
+	exit 1
+fi
+
 if [[ "${NENTRIES_INIT}" -ne 1 ]] ; then
 	echo "There must be only one entry to MPI_Init"
 	exit 1
