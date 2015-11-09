5489e0dc34eae88b40d33a9ad3afef30a3a4fbcc
Harald Servat <harald.servat@bsc.es>
2015-11-09 09:58:25 +0000

    Add helper functions

---
+++ tests/functional/helper_functions.bash
@@ -0,0 +1,46 @@
+
+#
+# Die with a message. die ("This was erroneous!")
+#
+
+die () { echo "$@" 1>&2; exit 1; }
+
+#
+# NumberEntriesInPRV (file.prv, type, value)
+#
+NumberEntriesInPRV ()
+{
+	if [[ $# -ne 3 ]]; then
+		die "Error! Invalid number of parameters in NumberEntriesInPRV"
+	fi
+
+	local n
+	if [[ -f ${1} ]]; then
+		n=`grep :${2}:${3} ${1} | wc -l`
+	else
+		n=0
+	fi
+
+	return $n
+}
+
+#
+# NumberEntriesInPCF (file.pcf, string)
+#
+NumberEntriesInPCF ()
+{
+	if [[ $# -ne 2 ]]; then
+		die "Error! Invalid number of parameters in NumberEntriesInPCF"
+	fi
+
+	local n
+	if [[ -f ${1} ]]; then
+		n=`grep ${2} ${1} | wc -l`
+	else
+		n=0
+	fi
+
+	return $n
+}
+
+
