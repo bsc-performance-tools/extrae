96a7fda586e26cb1d6f5fbab18014444d4897331
Harald Servat <harald.servat@bsc.es>
2015-11-11 09:11:56 +0000

    Forward LD_LIBRARY_PATH to binary

--- example/LINUX/CUDA/run_instrumented.sh
+++ example/LINUX/CUDA/run_instrumented.sh
@@ -2,4 +2,4 @@
 
 export EXTRAE_HOME=@sub_PREFIXDIR@
 
-EXTRAE_CONFIG_FILE=extrae.xml LD_LIBRARY_PATH=${EXTRAE_HOME}/lib: ./hello_instrumented
+EXTRAE_CONFIG_FILE=extrae.xml LD_LIBRARY_PATH=${EXTRAE_HOME}/lib:${LD_LIBRARY_PATH} ./hello_instrumented
