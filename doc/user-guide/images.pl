f743720474cc58e4a6a7f54cfa3ccef17466fe2d
Harald Servat <harald.servat@bsc.es>
2015-11-23 16:49:01 +0000

    Extend bug report

--- doc/user-guide/images.pl
+++ doc/user-guide/images.pl
@@ -2,19 +2,25 @@
 # Associate images original text with physical files.
 
 
-$key = q/{figure}{centering{{psfig{figure=imagesslashextrae-post-installation.eps,width=12cm}{{{{figure};LFS=11;FSF=1.6;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="542" HEIGHT="364" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="\begin{figure}\centering
-\psfig{figure=images/extrae-post-installation.eps, width=12cm}\end{figure}">|; 
-
 $key = q/{figure}{centering{{psfig{figure=overheadsslashoverheads,width=textwidth}{{{{figure};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="500" HEIGHT="316" BORDER="0"
  SRC="|."$dir".q|img5.png"
  ALT="\begin{figure}\centering
 \psfig{figure=overheads/overheads, width=\textwidth}\end{figure}">|; 
+
+$key = q/dollarast;MSF=1.6;LFS=11;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$\$\ast$">|; 
+
+$key = q/{figure}{centering{{psfig{figure=imagesslashextrae-post-installation.eps,width=12cm}{{{{figure};LFS=11;FSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="542" HEIGHT="364" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="\begin{figure}\centering
+\psfig{figure=images/extrae-post-installation.eps, width=12cm}\end{figure}">|; 
 
 $key = q/{landscape}%latex2htmlidmarker4340{par{table{{centerline{smalltabular{{p{7.5cm}poconfigure{sf{E}xtrae}({emcontinued})}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
@@ -28,12 +34,6 @@ $cached_env_img{$key} = q|<IMG
 ... to configure {\sf {E}xtrae} ({\em continued})}
 \end{table}\par
 \end{landscape}">|; 
-
-$key = q/dollarast;MSF=1.6;LFS=11;AAT/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="22" HEIGHT="36" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$\$\ast$">|; 
 
 $key = q/{landscape}%latex2htmlidmarker4246table{{centerline{smalltabular{{p{7.5cm}p{14cmablesavailabletoconfigure{sf{E}xtrae}}{table{{par{{landscape};LFS=11;FSF=1.6;AAT/;
 $cached_env_img{$key} = q|<IMG
