aec91c78e1f08df8f8b4e025747bf7d109cefdf4
Harald Servat <harald.servat@bsc.es>
2015-11-23 11:36:53 +0000

    Add submit bug report

--- doc/user-guide/internals.pl
+++ doc/user-guide/internals.pl
@@ -2,99 +2,7 @@
 # Associate internals original text with physical files.
 
 
-$key = q/cha:XML/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_CUDA/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:PackageDescription/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionMerge/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionOpenMP/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_DynInst_Intermediate_MPI/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:EnvVar/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:ProcessorPerformanceCounters/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:AdvancedJavaBindings/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionOthers/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:EnvVars_in_XML/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_AIX/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:FAQ/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:Examples_DynInst/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:Merging/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:API/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionUF/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_LDPRELOAD_Final/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionTraceConfiguration/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionSampling/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:CUDAinstrumentedroutines/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionDynamicMemory/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_static_link/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:Examples_DynInst_Intermediate/;
+$key = q/sec:XMLSectionIO/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
@@ -102,19 +10,23 @@ $key = q/sec:XMLSectionPerformanceCounters/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/subsec:Examples_static_Intermediate/;
+$key = q/sec:XMLSectionMPI/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/subsec:NetworkPerformanceCounters/;
+$key = q/subsec:OperatingSystemAccounting/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sec:OpenMPruntimesinstrumented/;
+$key = q/sec:XMLSectionSampling/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cha:InstrumentedRoutines/;
+$key = q/sec:XMLSectionOPENCL/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionTraceConfiguration/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
@@ -122,15 +34,195 @@ $key = q/fig:Overhead_Extrae_3_3_0/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sec:XMLSectionTraceControl/;
+$key = q/cha:InstrumentedRoutines/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cha:Examples/;
+$key = q/sec:XMLSectionMerge/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:OpenMPruntimesinstrumented/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:API/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:Introduction/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:FAQ/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:ProcessorPerformanceCounters/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:EnvVar/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:BasicAPI/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLIntelPEBS/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
 $key = q/subsec:Examples_Linux/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:Overhead/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:EnvironmentVariables/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:EnvironmentVariables_continued/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionDynamicMemory/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:XML/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:PackageDescription/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:AdvancedJavaBindings/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Configuration/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Examples_LDPRELOAD/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionBuffer/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_DynInst_Intermediate_MPI/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionBursts/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:JavaBindings/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Examples_static/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionUF/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:Examples_DynInst/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:wholeXML/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:Merging/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionCallers/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:CUDAinstrumentedroutines/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionOthers/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionOpenMP/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_static_link/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_LDPRELOAD_Final/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:MergerDescription/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:OPENCLinstrumentedroutines/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_static_Intermediate/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_CUDA/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionTraceControl/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:EnvVars_in_XML/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:Examples_DynInst_Intermediate/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionStorage/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:submitbug/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cha:QuickStart/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:ExtendedAPI/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/subsec:RunningTraceDynInst/;
+$ref_files{$key} = "$dir".q|user-guide.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sec:XMLSectionCUDA/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
@@ -142,19 +234,7 @@ $key = q/sec:MPIinstrumentedroutines/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/sec:Configuration/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionMPI/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:MergerDescription/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionCUDA/;
+$key = q/cha:Configuration/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
@@ -162,91 +242,15 @@ $key = q/subsec:RunningTraceNOTDynInst/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cha:Introduction/;
+$key = q/cha:Examples/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:EnvironmentVariables_continued/;
+$key = q/subsec:NetworkPerformanceCounters/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/cha:QuickStart/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionBuffer/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:RunningTraceDynInst/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:ExtendedAPI/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionBursts/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:EnvironmentVariables/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLIntelPEBS/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionIO/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:BasicAPI/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionStorage/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:Examples_static/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionOPENCL/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:wholeXML/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:Examples_LDPRELOAD/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/subsec:OperatingSystemAccounting/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:Overhead/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:JavaBindings/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:XMLSectionCallers/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/sec:OPENCLinstrumentedroutines/;
-$ref_files{$key} = "$dir".q|user-guide.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/cha:Configuration/;
+$key = q/subsec:Examples_AIX/;
 $ref_files{$key} = "$dir".q|user-guide.html|; 
 $noresave{$key} = "$nosave";
 
