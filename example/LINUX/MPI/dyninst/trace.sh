6581c06957d33f4aba06f8402c7bf420e1900104
Harald Servat <harald.servat@bsc.es>
2016-03-29 16:23:19 +0000

    Fix trace.sh within DynInst examples (CUDA+MPI and MPI)

--- example/LINUX/MPI/dyninst/trace.sh
+++ example/LINUX/MPI/dyninst/trace.sh
@@ -3,8 +3,8 @@
 export EXTRAE_HOME=@sub_PREFIXDIR@
 
 # Only show output for task 0, others task send output to /dev/null
-if test "${SLURM_PROCID}" == "0" -o
-        "${OMPI_COMM_WORLD_RANK}" == "0" -o
+if test "${SLURM_PROCID}" == "0" -o \
+        "${OMPI_COMM_WORLD_RANK}" == "0" -o \
         "${PMI_RANK}" == "0"; then
 	${EXTRAE_HOME}/bin/extrae -config ../extrae.xml $@
 else
