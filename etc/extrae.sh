b2a975a955efcb10dfe406b29bab16cc6d491e35
Lau Mercadal Melià <lau.mercadal@bsc.es>
2024-04-11 11:17:09 +0200

    Makes extrae.sh use EXTRAE_HOME value to set variables
    Change-Id: Id2eb812bed5f8558900979ec66016403b6dd9e50

--- etc/extrae.sh
+++ etc/extrae.sh
@@ -3,29 +3,24 @@
 # Edit with caution
 #
 
-EXTRAE_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-export EXTRAE_HOME=${EXTRAE_HOME}
+if [[ -z ${EXTRAE_HOME} ]]; then
+    export EXTRAE_HOME=@sub_PREFIXDIR@
+fi
 
-if test "${EXTRAE_HOME}" != "" ; then
-
-	# Read configuration variables if available!
-	if ! test -f ${EXTRAE_HOME}/etc/extrae-vars.sh ; then
-		echo "Error! Unable to locate ${EXTRAE_HOME}/etc/extrae-vars.sh"
-		echo "Dying..."
-		break
-	else
-		source ${EXTRAE_HOME}/etc/extrae-vars.sh
-	fi
-
-	if test -f "${EXTRAE_ONLINE_SOURCES}" ; then
-		source ${EXTRAE_ONLINE_SOURCES}
-	fi
-
+# Read configuration variables if available!
+if [[ -f ${EXTRAE_HOME}/etc/extrae-vars.sh ]] ; then
+	. ${EXTRAE_HOME}/etc/extrae-vars.sh
 else
-	echo "You have to define EXTRAE_HOME to run this script"
+	echo "Error! Unable to locate ${EXTRAE_HOME}/etc/extrae-vars.sh"
+	echo "Dying..."
+	exit
+fi
+
+if [[ -f "${EXTRAE_ONLINE_SOURCES}" ]] ; then
+	. ${EXTRAE_ONLINE_SOURCES}
 fi
 
 export PYTHONPATH=${EXTRAE_HOME}/libexec:${PYTHONPATH}
 
-export LD_LIBRARY_PATH=${EXTRAE_MPI_LIBSDIR}:${EXTRAE_PAPI_LIBSDIR}:${EXTRAE_LIBXML2_LIBSDIR}:${EXTRAE_DYNINST_LIBSDIR}:${EXTRAE_DWARF_LIBSDIR}:${EXTRAE_ELF_LIBSDIR}:${EXTRAE_LIBERTY_LIBSDIR}:${EXTRAE_BFD_LIBSDIR}:${EXTRAE_UNWIND_LIBSDIR}:${EXTRAE_BOOST_LIBSDIR}:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${EXTRAE_HOME}/lib:${EXTRAE_MPI_LIBSDIR}:${EXTRAE_PAPI_LIBSDIR}:${EXTRAE_LIBXML2_LIBSDIR}:${EXTRAE_DYNINST_LIBSDIR}:${EXTRAE_DWARF_LIBSDIR}:${EXTRAE_ELF_LIBSDIR}:${EXTRAE_LIBERTY_LIBSDIR}:${EXTRAE_BFD_LIBSDIR}:${EXTRAE_UNWIND_LIBSDIR}:${EXTRAE_BOOST_LIBSDIR}:${LD_LIBRARY_PATH}
 
