3d8295cf45c4bf7068decd29c96bf755216a496f
Julian Morillo <julian.morillo@bsc.es>
2024-06-19 15:25:51 +0200

    Test failed when PAPI_TOT_INS is not available in the testing system

--- tests/functional/xml/check_Extrae_xml_envvar_counters.sh
+++ tests/functional/xml/check_Extrae_xml_envvar_counters.sh
@@ -11,6 +11,19 @@ COUNTERS=PAPI_TOT_INS EXTRAE_CONFIG_FILE=extrae_envvar_counters.xml ./check_Extr
 ../../../src/merger/mpi2prv -f TRACE.mpits -o ${TRACE}.prv
 
 # Check
+if ! command -v papi_avail &> /dev/null
+then
+        echo "papi_avail could not be found"
+        exit 0
+fi
+
+PAPI_TOT_CYC_available=`papi_avail | grep PAPI_TOT_CYC | awk '{print $3}'`
+if [[ "$PAPI_TOT_CYC_available" == No ]]
+then
+        echo "PAPI_TOT_CYC is not available"
+        exit 0
+fi
+
 CheckEntryInPCF ${TRACE}.pcf PAPI_TOT_INS
 
 rm -fr TRACE* set-0 ${TRACE}.???
