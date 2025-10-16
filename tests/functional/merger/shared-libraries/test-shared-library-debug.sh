d1ab15ad17f21dcf1bf579599744f217666fc47e
Lau Mercadal Melià <lau.mercadal@bsc.es>
2025-10-16 12:08:54 +0200

    Change shared-libraries test to work with new address translation method

--- tests/functional/merger/shared-libraries/test-shared-library-debug.sh
+++ tests/functional/merger/shared-libraries/test-shared-library-debug.sh
@@ -8,12 +8,15 @@ EXTRAE_ON=1 ./main_debug
 ../../../../src/merger/mpi2prv -f TRACE.mpits
 
 # Do actual checks
-CheckEntryInPCF ${TRACE}.pcf "0 (Unresolved)"
-CheckEntryInPCF ${TRACE}.pcf "0 (_NOT_Found)"
-CheckEntryInPCF ${TRACE}.pcf "fA.c, libfA_debug.so"
-CheckEntryInPCF ${TRACE}.pcf "fB.c, libfB_debug.so"
-CheckEntryInPCF ${TRACE}.pcf "main.c, main_debug"
+CheckEntryInPCF ${TRACE}.pcf "fA.c:31.*libfA_debug.so"
+CheckEntryInPCF ${TRACE}.pcf "fB.c:31.*libfB_debug.so"
+CheckEntryInPCF ${TRACE}.pcf "main.c:33.*main_debug"
 
+NumberEntriesInPRV ${TRACE}.prv 60000119 2
+if [[ "${?}" -ne 1 ]] ; then
+	echo "There must be only one entry to 60000119:2"
+	exit 1
+fi
 NumberEntriesInPRV ${TRACE}.prv 60000119 3
 if [[ "${?}" -ne 1 ]] ; then
 	echo "There must be only one entry to 60000119:3"
@@ -22,11 +25,6 @@ fi
 NumberEntriesInPRV ${TRACE}.prv 60000119 4
 if [[ "${?}" -ne 1 ]] ; then
 	echo "There must be only one entry to 60000119:4"
-	exit 1
-fi
-NumberEntriesInPRV ${TRACE}.prv 60000119 5
-if [[ "${?}" -ne 1 ]] ; then
-	echo "There must be only one entry to 60000119:5"
 	exit 1
 fi
 NumberEntriesInPRV ${TRACE}.prv 60000119 0
--- tests/functional/merger/shared-libraries/test-shared-library-nodebug.sh
+++ tests/functional/merger/shared-libraries/test-shared-library-nodebug.sh
@@ -8,18 +8,11 @@ EXTRAE_ON=1 ./main_nodebug
 ../../../../src/merger/mpi2prv -f TRACE.mpits
 
 # Do actual checks
-CheckEntryInPCF ${TRACE}.pcf "0 (Unresolved)"
-CheckEntryInPCF ${TRACE}.pcf "0 (_NOT_Found)"
-CheckEntryInPCF ${TRACE}.pcf "main.c, main_nodebug"
+CheckEntryInPCF ${TRACE}.pcf "main.c:33.*main_nodebug"
 
-NumberEntriesInPRV ${TRACE}.prv 60000119 3
+NumberEntriesInPRV ${TRACE}.prv 60000119 2
 if [[ "${?}" -ne 1 ]] ; then
-	echo "There must be only one entry to 60000119:3"
-	exit 1
-fi
-NumberEntriesInPRV ${TRACE}.prv 60000119 1
-if [[ "${?}" -ne 2 ]] ; then
-	echo "There must be only two entries to 60000119:1"
+	echo "There must be only one entry to 60000119:2"
 	exit 1
 fi
 NumberEntriesInPRV ${TRACE}.prv 60000119 0
